065df2d3d7af83341f1c53b00ce513de3ccd7ec1
TdudeZ <71581792+tdudez@users.noreply.github.com>
2020-11-26 14:45:39 +0700

    Update Doing.java

--- Doing.java
+++ Doing.java
@@ -51,7 +51,7 @@ public class Doing {
             showjournal();
             System.out.print("pick book : ");
             pick = in.nextInt();
-            btext[i] = text[pick-1];
+            bjournal[i] = journal[pick-1];
             System.out.print("more ? (Y/N) : ");
             if (in.next().equalsIgnoreCase("n")) {
                 break;
